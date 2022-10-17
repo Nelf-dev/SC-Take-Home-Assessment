afeea9a9e8ce88133a37db4e16870b834caeb853
Nelson Feng <nelsonfeng98@gmail.com>
2022-10-17 13:20:19 +1100

    added tests in folders_test- checks if folders are loaded

--- folders/folders_test.go
+++ folders/folders_test.go
@@ -1,13 +1,39 @@
 package folders_test
 
-import (
-	"testing"
-	// "github.com/georgechieng-sc/interns-2022/folders"
-	// "github.com/stretchr/testify/assert"
-)
+//"github.com/georgechieng-sc/interns-2022/folders"
+//"github.com/stretchr/testify/assert"
+
+var req = &folders.FetchFolderRequest{
+	OrgID: uuid.FromStringOrNil(folders.DefaultOrgID),
+}
 
 func Test_GetAllFolders(t *testing.T) {
+	//Checks if struct outputs every value correctly
 	t.Run("test", func(t *testing.T) {
-		// your test/s here
+		output, _ := folders.GetAllFolders(req)
+		fmt.Print(output)
+		//I would like to check if each struct in FFR contains information, however I am not sure what to use to check if it is empty
+		for index, content in range output {
+			// output[index].id, name, orgID,deleted check if empty
+		}
+
 	})
 }
+
+func Test_FetchAllFoldersByOrgID(t *testing.T) {
+	t.Run("test", func(t *testing.T) {
+
+		//testing FetchAllfolders by OrgID if files are being appended into resfolder
+		const DefaultOrgID = "c1556e17-b7c0-45a3-a6ae-9546248fb17a"
+
+		orgId := uuid.FromStringOrNil(DefaultOrgID)
+		resfolder, _ := folders.FetchAllFoldersByOrgID(orgId)
+
+		if len(resfolder) <= 0 {
+			t.Error("Folders unable to append into resfolder")
+		} else {
+			t.Log("Folders appended into resfolder")
+		}
+	})
+}
+
